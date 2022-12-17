c51cdefb44455f21d3f00b8b0bac605aa64e5bbd
M Faried <m.a.faried@gmail.com>
2022-12-17 15:36:18 +0200

    Adding workbox and caching static resources

--- build/static/js/263.0de6dd62.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkindle_highlights=self.webpackChunkkindle_highlights||[]).push([[263],{263:function(i,e,h){h.r(e);h(313);var t=h(417);e.default=function(){return(0,t.jsx)("img",{src:"".concat("/Kindle-Highlights","/undraw_book_lover_mkck.svg"),alt:"Book Lovers",style:{width:"300px",height:"300px"}})}}}]);
