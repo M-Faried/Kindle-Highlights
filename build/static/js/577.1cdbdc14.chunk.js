033173ec4de3d2bbb07e7e74a7ba6e37d26a2e66
M Faried <m.a.faried@gmail.com>
2023-03-23 13:27:00 +0200

    Releasing version 3.3

---
+++ build/static/js/577.1cdbdc14.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkindle_highlights=self.webpackChunkkindle_highlights||[]).push([[577],{577:function(i,e,h){h.r(e);h(313);var s=h(417);e.default=function(){return(0,s.jsx)("img",{src:"".concat("/Kindle-Highlights","/images/undraw_book_lover_mkck.svg"),alt:"Book Lovers",style:{width:"300px",height:"300px"}})}}}]);
