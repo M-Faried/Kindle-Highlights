993defaaac98244118e2e38d2634a12540847315
M Faried <m.a.faried@gmail.com>
2023-03-23 13:27:17 +0200

    Updates

--- static/js/263.4344aee5.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkindle_highlights=self.webpackChunkkindle_highlights||[]).push([[263],{263:function(i,e,h){h.r(e);h(313);var s=h(417);e.default=function(){return(0,s.jsx)("img",{src:"".concat("/Kindle-Highlights","/images/undraw_book_lover_mkck.svg"),alt:"Book Lovers",style:{width:"300px",height:"300px"}})}}}]);
